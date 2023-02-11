f21b0e5359006310cf3bdf70563c02a363a36672
natxo <natxo@DESKTOP-7OCF2LG>
2023-02-11 16:21:01 +0100

    Segundo Commit

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -26,5 +26,12 @@ public class Main {
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
+        
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
     }
 }
